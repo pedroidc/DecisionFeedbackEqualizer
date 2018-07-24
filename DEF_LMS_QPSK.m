77fa40d9240d7b4bf5eed86725476ebb4359e7cc
Pedro Ivo <pi.cruz@outlook.com>
2018-07-24 13:50:21 -0300

    Uploading files

---
+++ DEF_LMS_QPSK.m
@@ -0,0 +1,137 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Simulação de um sistema de segurança utilizando precodificação não
+% linear, mais especificamente o precodificador Tomlinson-Harashima. O
+% precodificador, neste caso, tem como objetivo distorcer a informação de
+% forma que apenas o usuário que esteja recebendo através do canal
+% autêntico poderá decodificar a informação.
+%
+% Autor: Pedro Ivo da Cruz
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear variables; %close all;
+
+%% Simulation Parameters:
+M = 4;          % Ordem da modulação QPSK
+NTbits = 1e8;   % Número total de bits a serem transmitidos
+Nbits = 1e4;    % Número de bits a serem transmitidos por realização
+Ntr = 1000;     % Comprimento da sequência de treinamento em símbolos
+Ntrials = NTbits/Nbits; % Número total de realizações
+Ns = Nbits/log2(M);
+% u = 0.0001;
+
+%% QPSK Modulation:
+hMod = comm.RectangularQAMModulator( ...
+    'ModulationOrder', M, ...
+    'BitInput', true, ...
+    'NormalizationMethod', 'Average power');
+hDemod = comm.RectangularQAMDemodulator( ...
+    'ModulationOrder', M, ...
+    'BitOutput', true, ...
+    'NormalizationMethod', 'Average power');
+
+%% Channels parameters:
+SNR = 0:2:30;           % Signal-to-noise Ratio for simulation
+L = 6;                  % Channel length
+pp = exp(-(0:L-1)/2);   % Channel power profile with exponential decay with factor 2
+pp = pp.'/sum(pp);      % Normalize to make the total power 1;
+
+%% Initializations:
+hError = comm.ErrorRate('ResetInputPort', true);
+NSNR = length(SNR);
+berB = zeros(NSNR, 1);
+berE = zeros(NSNR, 1);
+ecma = zeros(Ns, 1);
+
+%% Simulation:
+cont = 0;
+barStr = 'SIM_QPSK_THP.m';
+progbar = waitbar(0, 'Initializing waitbar...', ...
+    'Name', barStr);
+
+fprintf('Simulação iniciada\n')
+
+for iSNR = 1:NSNR
+    
+    berBtrial = 0;
+    berEtrial = 0;
+    ecmatrial = zeros(Nbits, 1);
+    Ph = 0;
+    
+    for iReal = 1:Ntrials
+        
+        %% Transmitter:
+        xa = randsrc(Nbits, 1, [0 1]);          % Information bit stream
+        xamod = step(hMod, xa);                 % Modulate
+        
+        %%  Channel
+        hab = hba;                                % Reciprocal channel
+        ybch = filter(hab, 1, xamod);             % Apply fading
+        ybch = awgn(ybch, SNR(iSNR), 'measured'); % AWGN
+        
+        %% Receiver:
+        % DFE:
+        
+        
+        % Demodulate:
+        xbdemod = step(hDemod, ybd);
+        
+        % CMA Receiver:
+%         [w, ytemp, etemp] = cmaEqualizer(yech, u, 3*L, 0); % Apply CMA Equalizer
+%         yeeq = filter(w(:, end), 1, [yech; zeros(floor(3*L/2)-3, 1)]);
+%         yeeq(1:floor(3*L/2)-3) = [];
+%         yeeq = filter(w(:, end), 1, yech);
+%         xedemod = step(hDemod, yeeq); % Demodula
+        
+        
+        %% Performance Evaluation
+        % BER in B:
+        [~, bertemp] = biterr(xbdemod, xa);
+        berB(iSNR) = berB(iSNR) + bertemp;
+        
+        % CMA convergence:
+%         ecmatrial = ecmatrial + etemp.^2;
+        
+        % Simulation progress:
+        cont = cont + 1;
+        % Update bar:
+        prog = cont/(Ntrials*NSNR);
+        perc = 100*prog;
+        waitbar(prog, progbar, sprintf('%.2f%% Concluído', perc));
+        
+    end
+    
+end
+
+berB = berB/Ntrials;
+ecma = ecma/Ntrials;
+
+close(progbar)
+
+EbNo = SNR - 10*log10(log2(M));
+% BER teórica de um sistema QAM
+% berTheory = berawgn(EbNo, 'qam', M);
+berTheory = berfading(EbNo, 'qam', M, 1);
+% berTheory = berawgn(EbNo, 'psk', M, 'nondiff');
+
+% Plota os resultados
+figure
+semilogy(SNR, berB, 'bo--');
+hold on;
+semilogy(SNR, berE, 'ro--');
+semilogy(SNR, berTheory, 'kx-');
+legend('Alice', 'Eve', 'Theory');
+xlabel('SNR (dB)');
+ylabel('BER');
+%axis([EbNo(1) EbNo(end) 0.0001 1]);
+grid on;
+
+figure
+plot(10*log10(ecma));
+
+% scatterplot(yach)
+
+% fileStr = ['Resultados/THPrecoding_EveMODAdder_Nch' num2str(L)];
+
+% Salva dados para posterior utilização:
+% save(fileStr, ...
+%     'berB', 'berE', 'ecma', 'SNR', 'EbNo', 'L');
